9770ca6500012fb21d4c7b112ef066744af874f5
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-03-05 08:23:57 -0500

    Add css handling, finish basic functionality, and add small amount of expanatory content on each page

--- index.js
+++ index.js
@@ -2,73 +2,54 @@ const http = require("http");
 const fs = require("fs");
 
 const allFiles = fs.readdirSync("./");
-const allHtmlFiles= allFiles.filter((element) => {
-    if (element.endsWith('.html')){
-        return element;
-    } 
-const allCssFiles = allFiles.filter((element) =>
-{if (element.endsWith('.css') ) 
-return element;
-})
-
-    
-}).map(element => element.slice(0, -5))
-const port = process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-   
-
-
-
-    const requestedURL = req.url.slice(1);
-
-
-    if (requestedURL.endsWith(".css")) {
-      const cssFile = fs.readFileSync("./" + requestedURL, "utf8");
-      res.setHeader("Content-Type", "text/css");
-      res.end(cssFile);
+const allHtmlFiles = allFiles
+  .filter((element) => {
+    if (element.endsWith(".html")) {
+      return element;
     }
+  })
+  .map((element) => element.slice(0, -5));
 
+const allCssFiles = allFiles.filter((element) => {
+  if (element.endsWith(".css")) {
+    return element;
+  }
+});
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
 
-    
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html');
+  const requestedURL = req.url.slice(1);
 
-        if (requestedURL == "" ) {
-          let readStream =   fs.createReadStream('./index.html');
-          readStream.pipe(res);
+  if (requestedURL.endsWith(".css")) {
+    if (allCssFiles.includes(requestedURL)) {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/css");
+      const cssFile = fs.readFileSync(requestedURL, "utf8");
+      res.write(cssFile);
+    } else {
+      res.statusCode = 404;
+    }
+  }
+  if (requestedURL == "") {
+    res.statusCode = 200;
+    let readStream = fs.createReadStream("./index.html");
+    readStream.pipe(res);
+  } else if (allHtmlFiles.includes(requestedURL)) {
+    res.statusCode = 200;
+    let readStream = fs.createReadStream("./" + requestedURL + ".html");
 
+    readStream.on("open", function () {
+      readStream.pipe(res);
+    });
+  } else {
+    res.statusCode = 404;
+    const errorFile = fs.readFileSync("./404.html", "utf8");
 
-
-        } else {
-
-      let readStream =   fs.createReadStream("./" + requestedURL + '.html');
-
-        readStream.on('open', function () {
-          readStream.pipe(res);
-        });
-
-
-        readStream.on('error', ()=>{
-          res.statusCode = 404;
-        const errorFile = fs.readFileSync("./404.html", "utf8");
-      
-        res.end(errorFile);
-        
-
-        });
-
-      }
-
-
- 
-  
-})
-
-
-
-
+    res.end(errorFile);
+  }
+});
 
 server.listen(port, () => {
-  console.log(`Server running at port ${port}`)
-})
-
+  console.log(`Server running at port ${port}`);
+});
